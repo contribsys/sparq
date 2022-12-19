0968294bb65367579de74ee5d4220ed7d866f625
Mike Perham <mperham@gmail.com>
2022-12-19 14:05:31 -0800

    Add tests for login

--- public/web_test.go
+++ public/web_test.go
@@ -3,15 +3,18 @@ package public
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
+	"github.com/contribsys/sparq"
 	"github.com/contribsys/sparq/db"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPublicStatic(t *testing.T) {
-	stopper, err := db.TestDB("web")
+	stopper, err := db.TestDB("public")
 	assert.NoError(t, err)
 	defer stopper()
 
@@ -26,7 +29,7 @@ func TestPublicStatic(t *testing.T) {
 }
 
 func TestPublicWeb(t *testing.T) {
-	stopper, err := db.TestDB("web")
+	stopper, err := db.TestDB("public")
 	assert.NoError(t, err)
 	defer stopper()
 
@@ -44,6 +47,40 @@ func TestPublicWeb(t *testing.T) {
 	})
 }
 
+func TestPublicLogin(t *testing.T) {
+	stopper, err := db.TestDB("public")
+	assert.NoError(t, err)
+	defer stopper()
+	ts := &testSvr{}
+	root := rootRouter(ts)
+	AddPublicEndpoints(ts, root)
+
+	t.Run("login", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "http://localhost.dev:9494/login", nil)
+		w := httptest.NewRecorder()
+		root.ServeHTTP(w, req)
+		assert.Equal(t, 200, w.Code)
+
+		req = httptest.NewRequest("POST", "http://localhost.dev:9494/login", nil)
+		w = httptest.NewRecorder()
+		root.ServeHTTP(w, req)
+		assert.Equal(t, 200, w.Code)
+		assert.Contains(t, w.Body.String(), "Invalid username")
+
+		payload := url.Values{
+			"username": {"admin"},
+			"password": {"sparq123"},
+		}
+		req = httptest.NewRequest("POST", "http://localhost.dev:9494/login", strings.NewReader(payload.Encode()))
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		w = httptest.NewRecorder()
+		root.ServeHTTP(w, req)
+		assert.Equal(t, 302, w.Code)
+		assert.Contains(t, w.Header().Get("Location"), "/home")
+	})
+}
+
 func withQuery(query string, fn func(w *httptest.ResponseRecorder, req *http.Request)) {
 	req := httptest.NewRequest("GET", "http://localhost.dev:9494"+query, nil)
 	w := httptest.NewRecorder()
@@ -52,4 +89,11 @@ func withQuery(query string, fn func(w *httptest.ResponseRecorder, req *http.Req
 	AddPublicEndpoints(s, r)
 	r.ServeHTTP(w, req)
 	fn(w, req)
+}
+
+func rootRouter(s sparq.Server) *mux.Router {
+	root := mux.NewRouter()
+	store := &SqliteOauthStore{DB: s.DB()}
+	root.Use(BearerAuth(store))
+	return root
 }
